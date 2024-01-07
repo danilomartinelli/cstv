95ff839380226f258331feceb18657a33bb44915
Danilo Martinelli <danilomartinelli429@gmail.com>
2024-01-07 14:50:17 -0300

    refactor: clean unused files

---
+++ app/index.tsx
@@ -0,0 +1,41 @@
+import {Link} from 'expo-router';
+import {StyleSheet, View, Text} from 'react-native';
+
+export default function MainPage() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>List Matches Screen</Text>
+
+            <Link
+                href={{
+                    pathname: "/match-detail/[id]",
+                    params: {id: '1'}
+                }}
+                style={styles.link}
+            >
+                <Text style={styles.linkText}>Go to match detail</Text>
+            </Link>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    link: {
+        marginTop: 15,
+        paddingVertical: 15,
+    },
+    linkText: {
+        fontSize: 14,
+        color: '#2e78b7',
+    },
+});
---
+++ app/match-detail/[id].tsx
@@ -0,0 +1,35 @@
+import { router } from 'expo-router';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+
+export default function MatchDetailPage() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Match Detail Screen</Text>
+
+            <TouchableOpacity style={styles.link} onPress={() => router.back()}>
+                <Text style={styles.linkText}>Go back to list matches</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    link: {
+        marginTop: 15,
+        paddingVertical: 15,
+    },
+    linkText: {
+        fontSize: 14,
+        color: '#2e78b7',
+    },
+});
